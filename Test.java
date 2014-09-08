60c99c047b76bed9d61ba35e517a4d20789b3a85
Hallgeir Løkken <istarnion@gmail.com>
2014-09-08 08:33:51 +0200

    Update test.java. This version checks if touch sensor input works. Old motor test code is commented out.

--- Test.java
+++ Test.java
@@ -1,35 +1,18 @@
 import lejos.nxt.*;
 
 class Test {
-
-	int maxSpeed = 900;
-	int loSpeed = 100;
-
+	
 	public Test() throws Exception {
-		Motor.A.setSpeed(maxSpeed);
-		Motor.C.setSpeed(maxSpeed);
-
-		Motor.A.forward();
-		Motor.C.forward();
-
-		Thread.sleep(3000);
-/*
-*		Motor.A.setSpeed(loSpeed);
-*
-*		Thread.sleep(1000);
-*
-*		Motor.A.setSpeed(maxSpeed);
-*
-*		Thread.sleep(2000);
-*
-*		Motor.C.setSpeed(loSpeed);
-*
-*		Thread.sleep(1000);
-*
-*		Motor.C.setSpeed(maxSpeed);
-*
-*		Thread.sleep(2000);
-*/
+		while(!Button.ESCAPE.isPressed()) {
+			TouchSensor touch = new TouchSensor(SensorPort.S1);
+			TouchSensor touch2 = new TouchSensor(SensorPort.S2);
+			if(touch.isPressed()) {
+				System.out.println("Right sensor pushed!");
+			}
+			else if(touch2.isPressed()) {
+				System.out.println("Left sensor pressed!");
+			}
+		}	
 	}
 
 	public static void main(String[] args) throws Exception {
