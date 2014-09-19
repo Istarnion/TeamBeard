13b55f77f8501a1b49b3c179735aa36bd511b833
Hallgeir Løkken <istarnion@gmail.com>
2014-09-19 21:11:48 +0200

    Update the lego part of the music player

--- MusicPlayer.java
+++ MusicPlayer.java
@@ -4,18 +4,12 @@ import java.util.ArrayList;
 
 class MusicPlayer extends Thread {
 
-	private ArrayList<String> notes;
+	File f;
 
 	public MusicPlayer(String track) {
 		try {
-			File f = new File(track);
-			BufferedReader reader = new BufferedReader(
-				new InputStreamReader(new FileInputStream(f)));
-
-			notes = new ArrayList<String>();
-			for(String line=reader.readLine(); line != null; line=reader.readLine()) {
-				notes.add(line);
-			}
+			f = new File(track);
+			
 		}
 		catch(Exception e) {
 			e.printStackTrace();
@@ -24,17 +18,36 @@ class MusicPlayer extends Thread {
 
 	@Override
 	public void run() {
-		if(notes != null) {
-			int i;
-			for(String s : notes) {
-				i = s.indexOf(" ");
-				Sound.playTone(Integer.parseInt(s.substring(0, i)), Integer.parseInt(s.substring(i)));
+		if(f != null) {
+			boolean running = true;
+			BufferedReader reader;
+			while(running) {
 				try {
-					Thread.sleep(Integer.parseInt(s.substring(i)));
+					reader = new BufferedReader(new InputStreamReader(new FileInputStream(f)));
+					
+					int i = 0;
+					for(String s=reader.readLine(); (s != null && !s.isEmpty()); s=reader.readLine()) {
+						i = s.indexOf(" ");
+						int note = Integer.parseInt(s.substring(0, i));
+						if(note != 0) {
+							Sound.playTone(Integer.parseInt(s.substring(0, i)), Integer.parseInt(s.substring(i+1)));
+						}
+						try {
+							Thread.sleep(Integer.parseInt(s.substring(i+1)));
+						}
+						catch(InterruptedException e) {
+							System.out.println("Sleeping failed");
+							running = false;
+							System.exit(0);
+						}
+					}
+					reader.close();
 				}
-				catch(InterruptedException e) {
-					// Nothing
+				catch(Exception e) {
+					running =false;
+					e.printStackTrace();
 				}
+				Thread.yield();
 			}
 		}
 	}
