b81777396bbddbfd83ff6e4b2d4f253d78311862
Helblidi <gjermund.jevne@gmail.com>
2014-10-21 11:04:21 +0200

    Implemented menu

--- DrawingRobot/MenuState.java
+++ DrawingRobot/MenuState.java
@@ -1,22 +1,79 @@
-package drawingRobot;
+import lejos.nxt.*;
 
 
-/**
- * 
- * 
- * 
+
+/** This is the true menu, the one true menu, and the only true menu.
+ *  
+ *  One menu to rule them all, one menu to find them, 
+ *  one menu to bring them all, and in darkness bind them.
  */
 
-public class MenuState
-{
+public class MenuState implements State {
+	//options
+	private static int COPY=0, MUSIC=1, DRAW=2;
+	private int currOption=0;
+
+	private StateMachine sam;
 	/**
 	 * 
 	 * 
 	 * 
 	 */
-	public MenuState(){
-		super();
+	public MenuState(StateMachine sam){
+		this.sam=sam;
+		
+		
+		
 	}
+	private void waitForButtonPress(){
+		while(Button.ENTER.isUp() && Button.ESCAPE.isUp() && Button.LEFT.isUp() && Button.RIGHT.isUp()){
+			
+		}
+	}
+	
+	@Override
+	public void init() {
+		while(Button.ESCAPE.isUp()){
+			Thread.yield();
+			if(Button.ENTER.isDown()){
 
+			}
+			else if(Button.LEFT.isDown()){
+				currOption--;
+				if(currOption<0) currOption=2;
+				drawMenu();
+			}
+			else if(Button.RIGHT.isDown()){
+				currOption++;
+				if(currOption>2) currOption=0;
+				drawMenu();
+			}
+		}
+	}
+	private void drawMenu(){
+		LCD.clearDisplay();
+		if(currOption==COPY){
+			System.out.println(">Copy");
+		}
+		else{
+			System.out.println(" Copy");
+		}
+		if(currOption==MUSIC){
+			System.out.println(">Play Music");
+		}
+		else{
+			System.out.println(" Play Music");
+		}
+		if(currOption==DRAW){
+			System.out.println(">Draw");
+		}
+		else{
+			System.out.println(" Draw");
+		}
+	}
+	public void resume() {
+		
+	}
+	
 }
 
