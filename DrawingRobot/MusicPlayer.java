9660b8095a04e9df3becfd5c70bc34a0192ca3d5
Hallgeir Løkken <istarnion@gmail.com>
2014-10-21 18:16:58 +0200

    Added progress bar to MusicPlayer

--- DrawingRobot/MusicPlayer.java
+++ DrawingRobot/MusicPlayer.java
@@ -12,16 +12,20 @@ class MusicPlayer {
 	}
 
 	public static void play(int[][] composition) {
-		while(Button.ESCAPE.isUp()) {
-			for(int i = 0; Button.ESCAPE.isUp() && (i < composition[0].length); i++) {
-				Sound.playTone(composition[0][i], composition[1][i]);
-				try {
-					Thread.sleep(composition[1][i]);
-				} 
-				catch (InterruptedException e) {
-					System.out.println("Something went wrong, very very wrong. RUN!");
-				}
-			}				
-		}
+		LCD.clearDisplay();
+		for(int i = 0; Button.ESCAPE.isUp() && (i < composition[0].length); i++) {
+			Sound.playTone(composition[0][i], composition[1][i]);
+			try {
+				Thread.sleep(composition[1][i]);
+			} 
+			catch (InterruptedException e) {
+				System.out.println("Something went wrong, very very wrong. RUN!");
+			}
+
+			int progress = (i* (100/composition[0].length));
+			for(int x=0; x<10; x++) {
+				LCD.setPixel(progress, 55+x, 1);
+			}
+		}				
 	}
 }
